18d4a3b5f1c76fc1112a9412e7ff93413c0683de
sasoria <sergio.alberto.arevalo.soria@nav.no>
2024-01-10 17:49:06 +0100

    Oppdater workflow

--- src/components/Komponent.test.tsx
+++ src/components/Komponent.test.tsx
@@ -1,11 +1,11 @@
 import React from "react";
 import { describe, expect, it } from "vitest";
-import { fireEvent, render, screen } from "../utils/test-utils";
+import { render, screen } from "../utils/test-utils";
 import Komponent from "./Komponent";
 
 describe("Simple working test for Komponent", () => {
   it("has a text", () => {
-    render(<Komponent emoji="" tekst="tekst" />);
+    render(<Komponent tekst="tekst" />);
     expect(screen.getByText(/tekst/)).toBeDefined();
   });
 });
