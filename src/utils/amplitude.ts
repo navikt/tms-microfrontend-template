e2d66ec666f70a1db0fdacbee37b02d01402edbb
Amirhossien Lotfi <Amirhossien.lotfi@nav.no>
2025-01-29 14:18:36 +0100

    amplitude-logge via nav-dekoratoren-moduler slik at logging er i tråd med svaret bruker har avgitt i cookie-banner

--- src/utils/amplitude.ts
+++ src/utils/amplitude.ts
@@ -1,17 +1,7 @@
-import { init, track } from "@amplitude/analytics-browser";
+import { getAmplitudeInstance } from "@navikt/nav-dekoratoren-moduler";
 
-export const initAmplitude = () => {
-  init("default", undefined, {
-    useBatch: true,
-    serverUrl: "https://amplitude.nav.no/collect-auto",
-    ingestionMetadata: {
-      sourceName: window.location.toString(),
-    },
-  });
-};
+const logger = getAmplitudeInstance("dekoratoren");
 
 //TODO: komponent må fylles ut med et identifiserende navn for microfrontend
 //Default er at navnet på appen blir benyttet ved replace av "tms-microfrontend-template"
-export const logNavigereEvent = () => {
-  track("navigere", { komponent: "tms-microfrontend-template" });
-};
+export const logNavigereEvent = () => logger("navigere", { komponent: "tms-microfrontend-template" });
